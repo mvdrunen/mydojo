8971473df2f67a4d20894d88081d4bf15affb5de
marcel <mvdrunen@hotmail.com>
2017-01-31 12:15:53 +0100

    Second try

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/css/styles.css">
+		<title>MyDojo</title>
+	</head>
+
+	<body>
+		<?php
+		
+		$welkomstekst = "<h1>hello world</h1> \n";
+		echo $welkomstekst;
+			
+	    ?>
+	</body>
+</html>
